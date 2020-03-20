e1b33484df38f2db2ba01f6ec3c9ee11f8c79068
Hardik Sisodiya <hsshardiksisodiya0@gmail.com>
2020-03-20 23:13:47 +0530

    program-21

---
+++ Move_Zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        
+        int n = nums.length, count = 0;
+                
+        for(int i = 0; i<n; i++) {
+            if(nums[i] != 0) {
+                nums[count++] = nums[i];
+            }
+        }
+        while(count < n) {
+            nums[count++] = 0;
+        }
+    }
+}
