937b3b01f770b0a9d5365e0debc5806ca6c15813
Hardik Sisodiya <hsshardiksisodiya0@gmail.com>
2020-03-21 13:48:07 +0530

    Program-37

---
+++ First_Missing_Positive.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        
+        int val = 1;
+        Arrays.sort(nums);
+        
+        for(int i =0;i<nums.length;i++) {
+            if(nums[i]==val)
+                val+=1;
+        }
+        return val;
+    }
+}
