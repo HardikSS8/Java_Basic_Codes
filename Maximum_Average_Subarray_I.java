eb4fcb38192312afecfc1fa3a2ef6c89bb58a5d5
Hardik Sisodiya <hsshardiksisodiya0@gmail.com>
2020-03-25 16:23:48 +0530

    Program-41

--- Maximum_Average_Subarray_I.java
+++ Maximum_Average_Subarray_I.java
@@ -1,12 +1,10 @@
 public class Solution {
     public double findMaxAverage(int[] nums, int k) {
         
-        double sum=0;
-        double MAX_SUM = sum;
-        
+        double sum=0;   
         for(int i=0; i<k; i++)
             sum += nums[i];
-        
+        double MAX_SUM = sum;
         for(int i=k; i<nums.length; i++){
             sum += nums[i] - nums[i-k];
                 MAX_SUM = Math.max(MAX_SUM,sum);
