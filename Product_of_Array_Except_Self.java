685dec14ec732a752becbb58157e3a54e8aea969
Hardik Sisodiya <hsshardiksisodiya0@gmail.com>
2020-04-19 16:15:58 +0530

    Program-101

---
+++ Product_of_Array_Except_Self.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+
+        int n = nums.length;
+        int[] L = new int[n];
+        int[] R = new int[n];
+        int[] answer = new int[n];
+
+        L[0] = 1;
+        for (int i = 1; i < n; i++) {
+            L[i] = nums[i - 1] * L[i - 1];
+        }
+        R[n - 1] = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            R[i] = nums[i+1] * R[i+1];
+        }
+        for (int i = 0; i < n; i++) {
+            answer[i] = L[i] * R[i];
+        }
+        return answer;
+    }
+}
