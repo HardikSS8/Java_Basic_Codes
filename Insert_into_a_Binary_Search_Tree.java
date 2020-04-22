e072030956ee68f179946e175716556b2f7b595e
Hardik Sisodiya <hsshardiksisodiya0@gmail.com>
2020-04-22 15:50:51 +0530

    Program-106

---
+++ Insert_into_a_Binary_Search_Tree.java
@@ -0,0 +1,22 @@
+/*
+   public class TreeNode {
+       int val;
+       TreeNode left;
+       TreeNode right;
+       TreeNode(int x) { val = x; }
+   }
+ */
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root == null)
+            return new TreeNode(val);
+        else {
+            if(val < root.val) {
+                root.left = insertIntoBST(root.left, val);
+            } else {
+                root.right = insertIntoBST(root.right, val);
+            }
+        }
+        return root;
+    }
+}
