5c7fd0c687727d3dd8592a150cad1d33976a878b
Hardik Sisodiya <hsshardiksisodiya0@gmail.com>
2020-05-09 00:57:10 +0530

    Program-125

---
+++ Check_If_It_Is_a_Straight_Line.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean checkStraightLine(int[][] coordinates) {
+       
+        int x = coordinates[1][0] - coordinates[0][0];
+        int y = coordinates[1][1] - coordinates[0][1];
+        for(int i = 2; i < coordinates.length; i++)
+            if(x * (coordinates[i][1] - coordinates[0][1]) !=  y * (coordinates[i][0] - coordinates[0][0]))
+                return false;
+        return true;
+    }
+}
