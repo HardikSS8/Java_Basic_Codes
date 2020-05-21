8a35e09b5af0b2e4573981f7f90aca415a5c0a1a
Hardik Sisodiya <hsshardiksisodiya0@gmail.com>
2020-05-21 20:40:41 +0530

    Program-129

---
+++ Range_Sum_of_BST.java
@@ -0,0 +1,23 @@
+/*
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+ */
+class Solution {
+    int sum = 0;
+    
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        if(root != null) {
+        
+            if(root.val >= L && root.val <= R) {
+                sum += root.val;
+            }
+            rangeSumBST(root.left, L, R);
+            rangeSumBST(root.right, L, R);
+        }
+        return sum;
+    }
+}
