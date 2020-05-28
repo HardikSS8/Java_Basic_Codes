951aea25968905b594905e6f5e102f59cf645b32
Hardik Sisodiya <hsshardiksisodiya0@gmail.com>
2020-05-28 15:13:58 +0530

    Program-146

---
+++ Duplicate_Zeros.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+                
+        for(int i=0; i<arr.length-1; i++) {
+            if(arr[i] == 0) {
+                
+                for(int j=arr.length-1; j>i; j--) {
+                    arr[j] = arr[j-1];
+                }
+                i++;
+            }
+        }
+    }
+}
