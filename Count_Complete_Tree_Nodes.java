f40bcef60bb743954e6418268b8c81a4878b5021
Hardik Sisodiya <hsshardiksisodiya0@gmail.com>
2020-06-24 00:37:15 +0530

    Program-223

---
+++ Count_Complete_Tree_Nodes.java
@@ -0,0 +1,30 @@
+/*
+   Definition for a binary tree node.
+   public class TreeNode {
+       int val;
+       TreeNode left;
+       TreeNode right;
+       TreeNode() {}
+       TreeNode(int val) { this.val = val; }
+       TreeNode(int val, TreeNode left, TreeNode right) {
+           this.val = val;
+           this.left = left;
+           this.right = right;
+       }
+   }
+ */
+class Solution {
+    int count = 0;
+    
+    public int countNodes(TreeNode root) {
+        
+        if(root == null)
+            return count;
+        else {
+            count++;
+            count = countNodes(root.left);
+            count = countNodes(root.right);
+        }
+        return count;
+    }
+}
