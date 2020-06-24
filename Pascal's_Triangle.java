62a29bd79f1ec343b09e8f7ef5861f646d1dcbf0
Hardik Sisodiya <hsshardiksisodiya0@gmail.com>
2020-06-25 00:13:47 +0530

    Program-228

---
+++ Pascal's_Triangle.java
@@ -0,0 +1,24 @@
+class Solution {
+    
+    public List<List<Integer>> generate(int numRows) {
+	
+        List<List<Integer>> allrows = new ArrayList<List<Integer>>();
+
+        ArrayList<Integer> row = new ArrayList<Integer>();
+	
+        for(int i=0;i<numRows;i++) {
+            
+            row.add(0,1);
+            // System.out.println(row);
+            
+            for(int j=1;j<row.size()-1;j++)
+                row.set(j, row.get(j)+row.get(j+1));
+            
+            // System.out.println(row);
+            // System.out.println();
+            
+            allrows.add(new ArrayList<Integer>(row));
+	    }
+    	return allrows;	
+    }
+}
