eccbd98b92a775d9dad93a17c04685ba957ed16a
Hardik Sisodiya <hsshardiksisodiya0@gmail.com>
2020-07-02 14:04:06 +0530

    Program-242

---
+++ Isomorphic_Strings.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        
+        Map<Character, Character> map = new HashMap<>();
+        
+        for(int i=0; i<s.length(); i++) {
+            if(!map.containsKey(s.charAt(i))) {
+                
+                if(!map.containsValue(t.charAt(i))) {
+                    map.put(s.charAt(i), t.charAt(i));
+                } else {
+                    return false;
+                }
+            } else {
+                
+                if(map.get(s.charAt(i)) != t.charAt(i)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
