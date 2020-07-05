d58492918c1cc59fca2d3d3254d04000050b3e8f
Hardik Sisodiya <hsshardiksisodiya0@gmail.com>
2020-07-05 17:08:01 +0530

    Program-248

---
+++ Can_Make_Arithmetic_Progression_From_Sequence.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        
+        if(arr.length <= 2)
+            return true;
+        
+        Arrays.sort(arr);
+        
+        int d = arr[1] - arr[0];
+        for(int i=0; i<arr.length-1; i++) {
+            
+            if(arr[i+1] - arr[i] != d)
+                return false;
+        }
+        
+        return true;
+    }
+}
