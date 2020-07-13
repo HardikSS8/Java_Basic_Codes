8038a02d6da76fa8cec93935a558b53249be59c5
Hardik Sisodiya <hsshardiksisodiya0@gmail.com>
2020-07-13 10:07:46 +0530

    Program-251

---
+++ N-ary_Tree_Postorder_Traversal.java
@@ -0,0 +1,43 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<Integer> postorder(Node root) {
+        
+        LinkedList<Node> ll = new LinkedList<>();
+        
+        LinkedList<Integer> list = new LinkedList<>();
+        
+        if(root == null)
+            return list;
+        
+        ll.add(root);
+        
+        while(!ll.isEmpty()) {
+            Node node = ll.pollLast();
+            
+            list.addFirst(node.val);
+            
+            for(Node child : node.children)
+                ll.add(child);
+        }
+        
+        return list;
+    }
+}
