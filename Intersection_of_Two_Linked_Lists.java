bc7f145dc38b23b995bd255af7a037f9390e6a12
Hardik Sisodiya <hsshardiksisodiya0@gmail.com>
2020-08-17 13:06:41 +0530

    Program-287

---
+++ Intersection_of_Two_Linked_Lists.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        
+        ListNode pA = headA;
+        ListNode pB = headB;
+        
+        while(pA != pB) {
+            
+            pA = (pA != null) ? pA.next : headB;
+            
+            pB = (pB != null) ? pB.next : headA;
+        }
+        
+        return pA;
+    }
+}
