00f658d76745ec972a4c34e1c5d1b1d28b220db7
Hardik Sisodiya <hsshardiksisodiya0@gmail.com>
2020-10-19 17:33:54 +0530

    Program-319

---
+++ Occurrences_After_Bigram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String[] findOcurrences(String text, String first, String second) {
+     
+        String[] words = text.split(" ");
+        
+        ArrayList<String> list = new ArrayList<>();
+        
+        for(int i=0; i<words.length-2; i++) {
+            
+            if(words[i].equals(first) && words[i+1].equals(second))
+                list.add(words[i+2]);
+        }
+                
+        String[] ans = new String[list.size()];
+        
+        for(int i=0; i<list.size(); i++) {
+            ans[i] = list.get(i);
+        }
+        
+        return ans;
+    }
+}
