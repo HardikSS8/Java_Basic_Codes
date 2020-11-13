be56f407b48d1188130a9b3ab5cec682bd0ea6c8
Hardik Sisodiya <hsshardiksisodiya0@gmail.com>
2020-11-13 21:36:35 +0530

    Program-342

---
+++ Robot_Return_to_Origin.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        
+        int x = 0, y = 0;
+        
+        for(char c : moves.toCharArray()) {
+            
+            if(c == 'L') x--;
+            else if(c == 'R') x++;
+            else if(c == 'U') y++;
+            else y--;
+        }
+        
+        return (x == 0 && y == 0);
+    }
+}
