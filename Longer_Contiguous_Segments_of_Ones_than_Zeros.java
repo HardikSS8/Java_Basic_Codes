911c4ce2b94d492d0e9339fd4a371b988b05e723
Hardik Sisodiya <hsshardiksisodiya0@gmail.com>
2021-05-30 13:50:17 +0530

    Program-400

---
+++ Longer_Contiguous_Segments_of_Ones_than_Zeros.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean checkZeroOnes(String s) {
+        int count0 = 0, count1 = 0, max0 = 0, max1 = 0;
+        
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(s.charAt(i) == '0')
+            {
+                count1 = 0;
+                count0++;
+                max0 = Math.max(max0,count0);
+            }
+            else
+            {
+                count0 = 0;
+                count1++;
+                max1 = Math.max(max1,count1);
+            }
+           
+        }
+       
+       return max1 > max0;
+        
+    }
+}
